b917c3e7bc02fc39ed1f3882e2ef32f891da8265
Cyryl Płotnicki <cyplo@cyplo.net>
2017-01-09 15:52:20 +0000

    Fix bad request

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 call refreshenv
 (echo.) | func run echo
 echo.
-curl -f "http://localhost:7071/api/echo?badrequests"
+curl -f "http://localhost:7071/api/echo?name=world"
 
 if errorlevel 1 (
    exit /b %errorlevel%
