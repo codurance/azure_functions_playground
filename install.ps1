a0e6af20e6b57916a6556c5fe3723226bad268dc
Cyryl Płotnicki <cyplo@cyplo.net>
2017-01-09 14:30:08 +0000

    Add azure funtions install

--- install.ps1
+++ install.ps1
@@ -1,2 +1,3 @@
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco install -y visualstudiocode dotnet4.5 nodejs.install
+npm install -g yo generator-azurefunctions azure-functions-cli
