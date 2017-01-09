0eb689863b3f8d7d88adcd02a16a3d1875853ac0
Cyryl Płotnicki <cyplo@cyplo.net>
2017-01-09 15:59:07 +0000

    Basic echo server with local-only tests

--- build.bat
+++ build.bat
@@ -1,1 +1,9 @@
 call refreshenv
+(echo.) | func run echo
+echo.
+curl -f "http://localhost:7071/api/echo?name=world"
+
+if errorlevel 1 (
+   exit /b %errorlevel%
+)
+echo.
--- install.ps1
+++ install.ps1
@@ -1,3 +1,3 @@
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
-choco install -y dotnet4.5 nodejs.install
+choco install -y dotnet4.5 nodejs.install curl
 npm install -g yo generator-azurefunctions azure-functions-cli
